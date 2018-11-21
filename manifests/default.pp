86d182792902236eab44a6397ee95888b7e851d5
Mathieu Jadin <mathjadin@gmail.com>
2018-11-21 17:49:50 +0100

    Remove zlog dependency because it is now installed with srnmininet

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,12 +1,5 @@
 $default_path = "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
-$zlog_version = "1.2.12"
-$zlog_release_url = "https://github.com/HardySimpson/zlog/archive/${zlog_version}.tar.gz"
-$zlog_root_dir = "/home/vagrant"
-$zlog_source_path = "${zlog_root_dir}/zlog-${zlog_version}"
-$zlog_download_path = "${zlog_source_path}.tar.gz"
-$zlog_path = "/usr/local/lib/libzlog.so"
-
 $srnmininet_repo = "https://bitbucket.org/jadinm/srnmininet.git"
 $srnmininet_path = "/home/vagrant/srnmininet"
 $srn_bin_path = "/home/vagrant/srn/bin"
@@ -59,22 +52,6 @@ exec { 'srnmininet-provision':
 }
 
 # Project depedencies
-exec { 'zlog-download':
-  require => Exec['apt-update'],
-  creates => $zlog_source_path,
-  command => "wget -O - ${zlog_release_url} > ${zlog_download_path} &&\
-              tar -xvzf ${zlog_download_path} -C ${zlog_root_dir};"
-}
-exec { 'zlog':
-  require => [ Exec['apt-update'], Exec['zlog-download'] ],
-  cwd => $zlog_source_path,
-  creates => $zlog_path,
-  path => "${default_path}:${zlog_source_path}",
-  command => "make &&\
-              make install &&\
-	      /sbin/ldconfig -v &&\
-              rm ${zlog_download_path};"
-}
 package { 'libmnl-dev': }
 package { 'libnetfilter-queue-dev': }
 
