b4fbfbbb4009b165c66f342c4b4eb1add979d8b3
Mathieu Jadin <mathjadin@gmail.com>
2020-02-17 16:01:33 +0100

    Add graph to compare gamma value effect

--- test/repetita_plot.py
+++ test/repetita_plot.py
@@ -20,11 +20,23 @@ def parse_args():
                         help='The level of details in the logs.')
     parser.add_argument('--out-dir', help='Output directory root',
                         default='/root/graphs-ebpf/%s' % datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
-    parser.add_argument('--src-dir', help='Source directory root all test logs',
-                        default="/root/experiences")
+    parser.add_argument('--src-dirs', nargs='*', action='store',
+                        help='Source directories root all test logs',
+                        default=["/root/experiences",
+                                 "/root/paths-gamma-variable/no-ebpf"])
     parser.add_argument('--srmip-dir',
                         help='Source directory root all sr-mip optimization',
                         default="/root/maxflow-out")
+    parser.add_argument('--gamma-dirs', nargs='*', action='store',
+                        help='List of source directories for different '
+                             'values of gamma. Each element of the list has '
+                             'its gamma value appended to the list as well',
+                        default=["/root/paths-gamma-variable/ebpf-gamma-0.1",
+                                 0.1,
+                                 "/root/paths-gamma-variable/ebpf-gamma-0.5",
+                                 0.5,
+                                 "/root/paths-gamma-variable/ebpf-gamma-0.9",
+                                 0.9])
     return parser.parse_args()
 
 
@@ -107,6 +119,102 @@ def bw_ebpf_or_no_ebpf_by_topo(json_bandwidths, json_srmip_maxflow,
                 plt.close()
 
 
+def bw_gamma_by_topo(json_bandwidths, json_srmip_maxflow, output_path):
+
+    colors = [
+        "orangered",
+        "red",
+        "blue",
+        "green",
+        "black",
+        "yellow"
+    ]
+    markers = [
+        "o",
+        ".",
+        "^",
+        "v"
+    ]
+
+    topos = {}
+
+    for gamma, gamma_exp in json_bandwidths.items():
+        for topo, topo_exp in gamma_exp.items():
+            for demands, demands_exp in topo_exp.items():
+                for maxseg, maxseg_exp in demands_exp.items():
+                    topos.setdefault(topo, {}).setdefault(demands, {})\
+                        .setdefault(maxseg, {})[gamma] = maxseg_exp
+
+    for topo, topo_exp in topos.items():
+        for demands, demands_exp in topo_exp.items():
+            for maxseg, maxseg_exp in demands_exp.items():
+
+                figure_name = "bw_gamma_by_topo" \
+                              "_{topo}_{demands}"\
+                    .format(topo=topo, demands=demands)
+                fig = plt.figure()
+                subplot = fig.add_subplot(111)
+
+                i = 0
+                for gamma, gamma_exp in maxseg_exp.items():
+                    i += 1
+
+                    if i == 1:  # Add no ebpf
+                        times = []
+                        bw = []
+                        for t, b in sorted(gamma_exp[False]):
+                            times.append(t)
+                            bw.append(b)
+                        subplot.step(times, bw, color="#00B0F0",
+                                     marker="s", linewidth=LINE_WIDTH,
+                                     where="post", markersize=MARKER_SIZE,
+                                     zorder=i, label="No eBPF")
+
+                    # Get back data
+                    times = []
+                    bw = []
+                    for t, b in sorted(gamma_exp[True]):
+                        times.append(t)
+                        bw.append(b)
+                    subplot.step(times, bw, color=colors[i-1],
+                                 marker=markers[i-1], linewidth=LINE_WIDTH,
+                                 where="post", markersize=MARKER_SIZE,
+                                 zorder=i, label="Gamma %f" % gamma)
+
+                subplot.set_xlabel("Time (s)", fontsize=FONTSIZE)
+                subplot.set_ylabel("Bandwidth (Mbps)", fontsize=FONTSIZE)
+
+                maxseg_description = "with max {maxseg} segments"\
+                    .format(maxseg=maxseg) if int(maxseg) >= 0\
+                    else "without segment limit"
+                subplot.set_title("Bandwidth for {topo} - {demand}"
+                                  .format(topo=topo, demand=demands)
+                                  + maxseg_description)
+
+                # Add line for max value of maxflow if any
+                objective = json_srmip_maxflow.get(topo, {})\
+                    .get(demands, {}).get(6, None)  # TODO Change 6 by maxseg
+                if objective is not None:
+                    subplot.hlines(objective, 0, 100, colors="#009B55",  # Objective values are in kbps
+                                   linestyles="solid", label="optimum")
+                else:
+                    lg.error("No optimum computation found for '{topo}'"
+                             " - '{demands}' with maximum {maxseg} segments\n"
+                             .format(topo=topo, demands=demands, maxseg=maxseg))
+
+                lg.info("Saving figure for gammas for '{topo}' demand "
+                        "'{demands}' with maxseg={maxseg} to {path}\n"
+                        .format(topo=topo, maxseg=maxseg, demands=demands,
+                                path=os.path.join(output_path,
+                                                  figure_name + ".pdf")))
+                subplot.set_ylim(bottom=0)
+                subplot.set_xlim(left=1, right=MEASUREMENT_TIME)
+                fig.savefig(os.path.join(output_path, figure_name + ".pdf"),
+                            bbox_inches='tight', pad_inches=0, markersize=9)
+                fig.clf()
+                plt.close()
+
+
 def bw_ebpf_or_no_ebpf_aggregate(json_bandwidths, json_srmip_maxflow,
                                  output_path):
     color = "orangered"
@@ -115,18 +223,15 @@ def bw_ebpf_or_no_ebpf_aggregate(json_bandwidths, json_srmip_maxflow,
     bw_diff = []
     for topo, topo_exp in json_bandwidths.items():
         for demands, demands_exp in topo_exp.items():
-            print(demands)
             if "rand.1" not in demands:
                 # TODO Replace by plots by type of
                 #  demand files
                 continue
             for maxseg, maxseg_exp in demands_exp.items():
-                print(str(list(maxseg_exp.keys())))
 
                 if True not in maxseg_exp.keys() \
                         or False not in maxseg_exp.keys():
                     continue
-                print("HERE 2")
 
                 ebpf_example = np.median([x for _, x in maxseg_exp[True]])
                 no_ebpf_example = np.median([x for _, x in maxseg_exp[False]])
@@ -231,11 +336,36 @@ if __name__ == "__main__":
     lg.setLogLevel(args.log)
     os.mkdir(args.out_dir)
 
-    bw_loaded_data, snap_data = explore_bw_json_files(args.src_dir)
+    bw_loaded_data, snap_data = explore_bw_json_files(args.src_dirs)
     optim_bw_data = explore_maxflow_json_files(args.srmip_dir)
+    # Plot aggregates and comparison between ebpf topo or not
     bw_ebpf_or_no_ebpf_by_topo(bw_loaded_data, optim_bw_data, args.out_dir)
     bw_ebpf_or_no_ebpf_aggregate(bw_loaded_data, optim_bw_data, args.out_dir)
 
+    # Parse gamma diffs
+    gamma_loaded_data = {}
+    gamma_snap_data = {}
+    for i in range(0, len(args.gamma_dirs), 2):
+        bw_data, snaps = explore_bw_json_files([args.gamma_dirs[i]])
+        gamma_value = float(args.gamma_dirs[i+1])
+        gamma_loaded_data.setdefault(gamma_value, bw_data)
+        gamma_snap_data.setdefault(gamma_value, snaps)
+
+        # Add no ebpf data
+        for topo, topo_exp in gamma_loaded_data[gamma_value].items():
+            for demands, demands_exp in topo_exp.items():
+                for maxseg, maxseg_exp in demands_exp.items():
+                    bws_no_ebpf = bw_loaded_data.get(topo, {})\
+                        .get(demands, {}).get(maxseg, {}).get(False)
+                    if bws_no_ebpf is not None:
+                        maxseg_exp[False] = bws_no_ebpf
+                    else:
+                        print("Cannot find solution without ebpf for topo"
+                              " {topo} demand {demand}".format(topo=topo,
+                                                               demand=demands))
+    bw_gamma_by_topo(gamma_loaded_data, optim_bw_data, args.out_dir)
+
+    # Plot bw by topology
     for topo, topo_exp in bw_loaded_data.items():
         for demands, demands_exp in topo_exp.items():
             for maxseg, maxseg_exp in demands_exp.items():
