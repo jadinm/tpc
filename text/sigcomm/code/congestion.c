740592e4c359b832edbb6673fdf429e78c6b2e8e
Mathieu Jadin <mathjadin@gmail.com>
2020-02-19 10:39:55 +0100

    Add section for EXP3

--- text/sigcomm/code/congestion.c
+++ text/sigcomm/code/congestion.c
@@ -2,18 +2,17 @@ void react_to_congestion() {
 	// Wait exponential backoff
 	if (current_time - conn->last_move_time < conn->wait_before_move)
 		return;
-	// Select the path with the highest bandwidth
-	for (i = 0; i < MAX_SRH_BY_DEST; i++) {
-		srh_entry = &dst_infos->srhs[i];
-		current_bw = srh_entry->bw;
-		if (current_bw > highest_bw) {
-			best_srh = srh_entry->srh;
-			highest_bw = current_bw;
-		}
-	}
-	if (best_srh != null) {
-		bpf_setsockopt(skops, SOL_IPV6, IPV6_RTHDR, best_srh, sizeof(*best_srh));
-		conn->srh = best_srh;
-		update_backoff_timers(conn);
-	}
+	// We compute the reward and update EXP3 weight
+	theReward = reward(choice, t);
+	estimatedReward = 1.0 * theReward / probabilityDistribution[current_path];
+	weights[current_path] *= math.exp(estimatedReward * gamma / numActions); # important that we use estimated reward here!
+	// Select the path with EXP3
+	probabilityDistribution = distr(weights, gamma);
+	best_srh = draw(probabilityDistribution);
+	// Set the SRH
+	bpf_setsockopt(skops, SOL_IPV6, IPV6_RTHDR, best_srh, sizeof(*best_srh));
+}
+
+void distr(weights, gamma) {
+	return [(1.0 - GAMMA) * (w / sum(weights)) + (GAMMA / len(weights)) for w in weights]
 }
